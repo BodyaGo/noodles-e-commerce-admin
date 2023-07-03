02a3eb4428a2a2b30a2fb47777a80e0c0243e8ad
BodyaGo <mukhabodya@gmail.com>
2023-07-04 01:21:48 +0300

    upgrade products

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,7 +1,18 @@
-export default function handler(req, res) {
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+
+export default async function handler(req, res) {
     const {method} = req;
+
     if(method === 'POST'){
-        
+        const {title, description, price} = req.body;
+        await mongooseConnect();
+        const productDoc = await Product.create({
+            title,
+            description,
+            price,
+        })
+        res.json(productDoc);
     }
 
 }
