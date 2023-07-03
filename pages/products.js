c9d729d92e8e19dcd9b44292d628262338b4c991
BodyaGo <mukhabodya@gmail.com>
2023-07-04 00:48:31 +0300

    upgrede products page

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,10 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products(){
     return(
         <Layout>
-            Products page
+            <Link className="bg-green-600 text-white rounded-md py-1 px-2" href={'/products/new'}>Add new product</Link>
         </Layout>
     );
 }
